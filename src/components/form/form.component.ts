8d881be07ce37da4dd2fa2d0e1984a2b14c20031
Akshaykumar Sarsamkar <Akshaykumar Sarsamkar@ho.rnt.ai>
2026-02-14 18:40:24 +0530

    ReactiveForm

--- src/components/form/form.component.ts
+++ src/components/form/form.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+
+  //----- using formControl ----------
+
+  // userData = {
+  //   firstName: '',
+  //   lastName: '',
+  //   middleName: ''
+  // }
+
+  // firstName = new FormControl();
+  // lastName = new FormControl();
+  // middleName = new FormControl()
+
+
+  // onSubmit() {
+  //   console.log(this.firstName.value)
+  //   console.log(this.lastName.value)
+  //   console.log(this.middleName.value)
+
+  //   this.userData.firstName = this.firstName.value;
+  // }
+
+
+
+  // ----------- GroupControl ----------------
+  userForm = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    middleName: new FormControl(),
+  })
+
+
+  submit() {
+    console.log(this.userForm.value)
+    this.userForm.setValue({
+      firstName: '',
+      lastName: '',
+      middleName: ''
+    });
+  }
+
 
 }
